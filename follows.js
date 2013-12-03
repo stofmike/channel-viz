10fdda794f9044d89b604793c7d7cc964b5ea16b
stofmike <stofmike@yahoo.co.uk>
2013-12-03 09:00:01 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'A8fC0BnBAs6dgxAqDF5sjLpOr7VKGLv49PUUSQ32nABRgLTa', // Unique master Xively API key to be used as a default
+		defaultFeeds	= ['79508344'], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Application Name', // Replaces Xively logo in the header
+		dataDuration	= '6days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
@@ -348,7 +348,7 @@
 
 					$('#feed-' + data.id + ' .duration-90').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
-						updateFeeds(data.id, thisFeedDatastreams, '90days', 10800);
+						updateFeeds(data.id, thisFeedDatastreams, '1Year', 43200);
 						return false;
 					});
 
